5908f0f23504a802f3fc1b35b2ffca01b418a483
Muhamad Nur <48532204+nurd0tid@users.noreply.github.com>
2022-06-15 11:20:12 +0700

    Update apiResi.js

--- routes/apiResi.js
+++ routes/apiResi.js
@@ -19,19 +19,4 @@ router.get('/tracking&courier=:kurir&awb=:noresi', (req, res) => {
     .catch(err => res.send(err))
 })
 
-// var options = {
-//   method: 'GET',
-//   url: 'https://api.aftership.com/v4/trackings/anteraja/10004709727433',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'aftership-api-key': '9f72c343-eb94-479f-9c1a-9ee95054eb39'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-//   console.log(response.data);
-// }).catch(function (error) {
-//   console.error(error);
-// });
-
 module.exports = router
